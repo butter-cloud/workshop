084e727d093d72eff2b8ea57eb3411938cfdb6dd
leejay210 <leejay@lgupluspartners.co.kr>
2025-01-10 11:31:14 +0900

    init spring boot app

---
+++ src/test/java/com/workshop/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workshop.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
